e80d679c9cb693c66e33089968e83725bed225f1
Helena <v0robjeva.alena@yandex.ru>
2023-03-12 20:54:05 +0300

    API/C33

---
+++ src/test/java/Test1.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class Test1 {
+    @Test
+    void shouldReturnBody() {
+
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("11.11.23") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("11.11.23"))
+        ;
+    }
+}
